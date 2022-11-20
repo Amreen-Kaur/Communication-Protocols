2c209b91cd8e9ad4412866f5f57173a8445e3f46
Amreen-Kaur <97379008+Amreen-Kaur@users.noreply.github.com>
2022-11-20 18:33:25 +0530

    Update UARTV.v

--- UARTV.v
+++ UARTV.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps 
  
 module top(
-    input clk,
-    input start,
-    input [7:0] txin,
+    input clk,//tr
+    input start,//tr
+ input [7:0] txin,//input data to be sent
     output reg tx, 
     input rx,
-    output [7:0] rxout,
-    output rxdone, txdone
+ output [7:0] rxout, //output seen from reciever
+    output rxdone, txdone //flags
     );
     
  parameter clk_value = 100_000;
  parameter baud = 9600;
- 
  parameter wait_count = clk_value / baud;
  
- reg bitDone = 0;
- integer count = 0;
+ 
+ reg bitDone = 0;//tr- to reach the point where value is to be transmitted
+ integer count = 0;//tr
  parameter idle = 0, send = 1, check = 2;
  reg [1:0] state = idle;
  
@@ -39,26 +39,26 @@ module top(
        bitDone <= 1'b0;  
       end    
   end
- 
  end
  
  ///////////////////////TX Logic
  reg [9:0] txData;///stop bit data start
- integer bitIndex = 0; ///reg [3:0];
+ integer bitIndex = 0;
  reg [9:0] shifttx = 0;
  
  
  always@(posedge clk)
  begin
  case(state)
- idle : 
+  
+ idle : //setup
      begin
-           tx       <= 1'b1;
+           tx       <= 1'b1;//start the process transmit high      
            txData   <= 0;
            bitIndex <= 0;
            shifttx  <= 0;
            
-            if(start == 1'b1)
+      if(start == 1'b1)//outside signal
               begin
                 txData <= {1'b1,txin,1'b0};
                 state  <= send;
@@ -69,16 +69,14 @@ module top(
               end
      end
  
-  send: begin
+  send: begin //to send the setup data
            tx       <= txData[bitIndex];
            state    <= check;
            shifttx  <= {txData[bitIndex], shifttx[9:1]};
   end 
   
-  check: 
-  begin
-       
-           
+  check: //checks the status of all the helpers for further processing
+  begin       
                if(bitIndex <= 9) ///0 - 9 = 10
                   begin
                     if(bitDone == 1'b1)
@@ -95,20 +93,18 @@ module top(
             end
  
  default: state <= idle;
- 
  endcase
- 
  end
  
 assign txdone = (bitIndex == 9 && bitDone == 1'b1) ? 1'b1 : 1'b0;
  
- 
  ////////////////////////////////RX Logic
- integer rcount = 0;
- integer rindex = 0;
+ integer rcount = 0;//no of clock ticks elapsed so far
+ integer rindex = 0;//incoming  bitcount
  parameter ridle = 0, rwait = 1, recv = 2, rcheck = 3;
  reg [1:0] rstate;
- reg [9:0] rxdata;
+ reg [9:0] rxdata;//complete bitstream
+ 
  always@(posedge clk)
  begin
  case(rstate)
@@ -118,7 +114,7 @@ assign txdone = (bitIndex == 9 && bitDone == 1'b1) ? 1'b1 : 1'b0;
       rindex <= 0;
       rcount <= 0;
         
-         if(rx == 1'b0)
+      if(rx == 1'b0)// indication of beginning of recieving
           begin
            rstate <= rwait;
           end
@@ -160,18 +156,13 @@ begin
         rindex <= 0;
         end
 end
- 
- 
-default : rstate <= ridle;
- 
- 
+  
+default : rstate <= ridle; 
  endcase
  end
  
- 
 assign rxout = rxdata[8:1]; 
 assign rxdone = (rindex == 9 && bitDone == 1'b1) ? 1'b1 : 1'b0;
- 
  
  endmodule
  //////////////////////////////////
